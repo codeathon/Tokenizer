4da83c76b1c526246a3483a3da0d2b71a07558dc
codeathon <rohit.ainapure@gmail.com>
2012-05-23 14:02:12 -0400

    token.pl modified & output file added

--- token.pl
+++ token.pl
@@ -3,16 +3,22 @@ use strict;
 use warnings;
 
 open FILE, "<perl" || die "Cannot open the test file.";
+open W_FILE, ">>output_token.txt" || die "Cannot open/create the output file";
+
 my $total_words = 0;
 print "\nCode to count the total number of words in the text file using different delimiters\n";
 
 while(my $line=<FILE>) {
-	my $words = split(/([" "+-,;])/, $line);
-	my $t_words = $words;
+	my @words_line = split(/([+ - , ; .])/, $line);
+	foreach my $token (@words_line) {
+		print W_FILE "$token \n";
+		
+	}
+	my $word_count = split(/([+ - , ; . ])/, $line);
+	my $t_words = $word_count;
 	$total_words = $total_words + $t_words; 
 }
 
 print "\nThe total no of words are : $total_words \n";
   
 exit 0;
-  
