b4d1db73b1fbd41b80d27c5377e1bf7deafd95a1
codeathon <rohit.ainapure@gmail.com>
2012-05-23 14:58:32 -0400

    output_datastructures.txt added

--- datastructures.pl
+++ datastructures.pl
@@ -2,26 +2,28 @@
 
 # Stack & its operations 
 
-print "Create / Initialize a stack \n\n";
+open (D_FILE, ">>output_datastructures.txt");
+
+print D_FILE "Create / Initialize a stack \n\n";
 
 @stack = qw( Integer variables );						# Quote word used to generate a list of words.
 
-print "Stack before pushing any value :\n  @stack \n";
+print D_FILE "Stack before pushing any value :\n  @stack \n";
 
 push (@stack, "1: Perl was originally named Pearl. \n");
 
-print "Stack content after Item is pushed:\n  @stack \n";
+print D_FILE "Stack content after Item is pushed:\n  @stack \n";
 
 $var = "2: Perl is a general-purpose programming language originally developed for text manipulation. \n";
 
 push (@stack, $var);
 
-print "Push item on stack:\n  @stack \n";
+print D_FILE "Push item on stack:\n  @stack \n";
 
 $top = pop @stack;
 
-print "Top of stack, popped:  $top\n";
+print D_FILE "Top of stack, popped:  $top\n";
 
-print "What is left of the stack:\n  @stack \n\n";
+print D_FILE "What is left of the stack:\n  @stack \n\n";
 
 exit(0);
