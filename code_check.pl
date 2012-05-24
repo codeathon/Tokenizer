5207be60052cf505dee487c3a069470c3256fd42
codeathon <rohit.ainapure@gmail.com>
2012-05-24 19:33:16 -0400

    code check complete with output file

--- code_check.pl
+++ code_check.pl
@@ -7,21 +7,19 @@ sub check_line {		# Checks each line of the file for a faulty if statement
     # Check for that =.
     if($line =~ /^\s*if\s*\(.*[^!<>=]=([^=].*\)|\))/) {
 	if(!$badfound) {
-	    print("Following were the faulty if statements:\n");
+	    print OUT "Following were the faulty if statements:\n";
 	    $badfound = 1;
 	}
-	print "$fn:$.: $line\n";
+	print OUT "$fn:$.: $line\n";
     }
 }
 
 sub check_file {		# Open the file and call check_line for each line in the file
     my($fn) = @_;
 
-    if(!open(IN, $fn)) {
-	print "Cannot read $fn.\n";
-	return;
-    }
-
+    open IN, "<test.c" || die "Cannot read $fn.\n";
+    open OUT, ">>output_codecheck" || die "Cannot create output_codecheck";
+    print OUT "file opened \n";
     my($line);
     while($line = <IN>)
     {
@@ -32,7 +30,5 @@ sub check_file {		# Open the file and call check_line for each line in the file
     close IN;
 }
 
-while(my $fn = shift @ARGV) {			# From the command promp, check each argument list
-    check_file($fn);
-}
-if(!$badfound) { print "No suspicious lines were found.\n"; }
+check_file();
+if(!$badfound) { print OUT "No suspicious lines were found.\n"; close OUT;}
---
+++ test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+        int x,y;
+        printf("Enter value for x :");
+        scanf("%d",&x);
+        printf("Enter value for y :");
+        scanf("%d",&y);
+        if ( x = y ){
+                printf("X is large number - %d\n",x);
+        }
+        else{
+                printf("Y is large number - %d\n",y);
+        }
+        return 0;
+}
