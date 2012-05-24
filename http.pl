4702cf54359460bdb15c029fd034275d1bea4bda
codeathon <rohit.ainapure@gmail.com>
2012-05-24 16:35:44 -0400

    http modified

--- http.pl
+++ http.pl
@@ -6,10 +6,10 @@ use LWP::UserAgent;
 
 my $url = 'http://www.synacor.com';
 
-my $ua = LWP::UserAgent->new;
-my $response = $ua->get( $url );
+my $user_agent = LWP::UserAgent->new;
+my $response = $user_agent->get( $url );
 
 $response->is_success or
-    die "Failed to GET '$url': ", $response->status_line;
+    die "Unsuccessfull attempt to do a GET on '$url': ", $response->status_line;
 
 print $response->as_string;
